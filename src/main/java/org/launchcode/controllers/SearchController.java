f5dd1dbcceca810930aa24f7a5b194f7aa087e3d
Vishnupriya <priyamrvishnu@gmail.com>
2019-01-01 18:39:11 -0600

    final code

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -24,20 +24,20 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
-    @RequestMapping(value = "results", method = RequestMethod.POST)
+    @RequestMapping(value = "results", method = RequestMethod.GET)
     public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
 
         if (searchType.equals("all")) {
             ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
 
-            model.addAttribute(jobs);
+            model.addAttribute("jobs",jobs);
             return "search";
 
         } else {
             ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
 
             model.addAttribute("title", "Jobs for " + searchTerm);
-            model.addAttribute( jobs);
+            model.addAttribute( "jobs", jobs);
 
             return "search";
         }
